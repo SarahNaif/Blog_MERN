96668a7cb398284fb62285a5f12dfc48ea3bcb2a
SarahNaif <sarah.althowebi@gmail.com>
2023-04-26 11:08:11 +0300

    add profile route + upload pic

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
-
+import uploadPicture from "../middlewares/uploadPictureMiddleware.js"
+import fileRemover from "../utils/fileRemover.js"
 
 const registerUser = async (req, res, next) => {
     try {
@@ -9,19 +10,18 @@ const registerUser = async (req, res, next) => {
         name: req.body.name,
     };
       newUser.email = newUser.email.toLowerCase();
-      // check whether the user exists or not
       let user = await User.findOne({ email:newUser.email });
   
       if (user) {
         throw new Error("User have already registered");
       }
   
-      // creating a new user
       user = await User.create(newUser);
   
       return res.status(201).json({
         msg: "successfully registered",
         token: await user.generateJWT(),
+
       });
 
     } catch (error) {
@@ -59,7 +59,14 @@ const userProfile = async (req, res, next) => {
       let user = await User.findById(req.user._id);
   
       if (user) {
-        return res.status(201).json({user});
+        return res.status(201).json({
+          _id: user._id,
+          avatar: user.avatar,
+          name: user.name,
+          email: user.email,
+          verified: user.verified,
+          admin: user.admin,
+        });
       } else {
         let error = new Error("User not found");
         error.statusCode = 404;
@@ -70,7 +77,8 @@ const userProfile = async (req, res, next) => {
     }
   };
 
-  const updateProfile = async (req, res, next) => {
+
+const updateProfile = async (req, res, next) => {
     try {
       let user = await User.findById(req.user._id);
   
@@ -80,6 +88,7 @@ const userProfile = async (req, res, next) => {
   
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
+
       if (req.body.password && req.body.password.length < 6) {
         throw new Error("Password length must be at least 6 character");
       } else if (req.body.password) {
@@ -89,6 +98,7 @@ const userProfile = async (req, res, next) => {
       const updatedUserProfile = await user.save();
   
       res.json({
+        msg: "Profile Updated",
         _id: updatedUserProfile._id,
         avatar: updatedUserProfile.avatar,
         name: updatedUserProfile.name,
@@ -104,8 +114,67 @@ const userProfile = async (req, res, next) => {
 
 
 
+const updateProfilePicture = async (req, res, next) => {
+    try {
+      const upload = uploadPicture.single("profilePicture");
+  
+      upload(req, res, async function (err) {
+        if (err) {
+          const error = new Error(
+            "An unknown error occured when uploading " + err.message
+          );
+          next(error);
+        } else {
+          if (req.file) {
+            let filename;
+            let updatedUser = await User.findById(req.user._id);
+            filename = updatedUser.avatar;
+            if (filename) {
+              fileRemover(filename);
+            }
+            updatedUser.avatar = req.file.filename;
+            await updatedUser.save();
+            res.json({
+              _id: updatedUser._id,
+              avatar: updatedUser.avatar,
+              name: updatedUser.name,
+              email: updatedUser.email,
+              verified: updatedUser.verified,
+              admin: updatedUser.admin,
+              token: await updatedUser.generateJWT(),
+            });
+          } else {
+            let filename;
+            let updatedUser = await User.findById(req.user._id);
+            filename = updatedUser.avatar;
+            updatedUser.avatar = "";
+            await updatedUser.save();
+            fileRemover(filename);
+            
+            res.json({
+              _id: updatedUser._id,
+              avatar: updatedUser.avatar,
+              name: updatedUser.name,
+              email: updatedUser.email,
+              verified: updatedUser.verified,
+              admin: updatedUser.admin,
+              token: await updatedUser.generateJWT(),
+            });
+          }
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+
+
+
+
   export {registerUser ,
     loginUser,
     userProfile,
     updateProfile,
+    updateProfilePicture,
   };
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,15 +4,17 @@ import {
     loginUser,
     userProfile,
     updateProfile,
+    updateProfilePicture,
   } from "../controllers/userController";
-
+import {authGuard} from "../middlewares/verifyAuthToken"
 const router = express.Router();
 
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
-router.get("/profile",userProfile);
-router.put("/updateProfile", updateProfile);
+router.get("/profile",authGuard,userProfile);
+router.put("/updateProfile", authGuard,updateProfile);
+router.put("/updateProfilePicture", authGuard, updateProfilePicture);
 
 
 export default router;
