64d093352e592d5349d94d68387ec8bc5c06dd57
SarahNaif <sarah.althowebi@gmail.com>
2023-05-09 18:03:48 +0300

    feat - add login functionality

--- client/src/pages/Auth/LoginPage.jsx
+++ client/src/pages/Auth/LoginPage.jsx
@@ -1,13 +1,64 @@
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+
+
 import MainLayout from "../../components/navigation/MainLayout"
+import { login } from "../../services/index/users";
+import { userActions } from "../../store/reducers/userReducers";
 
 
 const LoginPage = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userState = useSelector((state) => state.user);
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({  email, password }) => {
+      return login({  email, password });
+    },
+    onSuccess: (data) => {
+      dispatch(userActions.setUserInfo(data));
+      console.log(data);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
+  useEffect(() => {
+    if (userState.userInfo) {
+      navigate("/");
+    }
+  }, [navigate, userState.userInfo]);
+
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const submitHandler = (data) => {
+    const {email, password } = data;
+    mutate({ email, password });
+  };
+
   return (
      <MainLayout>
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-10">
-
-        <div className="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
+      <div className="mt-24 flex flex-col items-center justify-center bg-gray-10">
+        <div className="flex flex-col bg-white shadow-lg px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
           <div className="font-medium self-center text-xl sm:text-2xl uppercase text-gray-800">
             Login To Your Account
           </div>
@@ -22,10 +73,11 @@ const LoginPage = () => {
             </div>
           </div>
           <div className="mt-10">
-            <form action="#">
+            <form onSubmit={handleSubmit(submitHandler)}>
+              
               <div className="flex flex-col mb-6">
                 <label
-                  for="email"
+                  htmlFor="email"
                   className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
                 >
                   E-Mail Address:
@@ -48,15 +100,29 @@ const LoginPage = () => {
                   <input
                     id="email"
                     type="email"
-                    name="email"
-                    className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+                    {...register("email", {
+                      pattern: {
+                        value:
+                          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: "Enter a valid email",
+                      },
+                      required: {
+                        value: true,
+                        message: "Email is required",
+                      },
+                    })}
+                    className={`text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400  w-full py-2 focus:outline-none  ${errors.email ?  "focus:border-red-500 border-red-500" : "focus:border-blue-400 "}`}
                     placeholder="E-Mail Address"
                   />
                 </div>
+                {errors.email?.message && (
+                  <p className="text-red-500 text-xs mt-1"> {errors.email?.message}</p>
+                )}
               </div>
+
               <div className="flex flex-col mb-6">
                 <label
-                  for="password"
+                  htmlFor="password"
                   className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600"
                 >
                   Password:
@@ -81,11 +147,25 @@ const LoginPage = () => {
                   <input
                     id="password"
                     type="password"
-                    name="password"
-                    className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400"
+                    {...register("password", {
+                      required: {
+                        value: true,
+                        message: "Password is required",
+                      },
+                      minLength: {
+                        value: 6,
+                        message: "Password length must be at least 6 characters",
+                      },
+                    })}
+                    className={`text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400  w-full py-2 focus:outline-none  ${errors.password ?  "focus:border-red-500 border-red-500" : "focus:border-blue-400 "}`}
                     placeholder="Password"
                   />
                 </div>
+                {errors.password?.message && (
+                <p className="text-red-500 text-xs mt-1">
+                  {errors.password?.message}
+                </p>
+              )}
               </div>
 
               <div className="flex items-center mb-6 -mt-4">
@@ -102,7 +182,9 @@ const LoginPage = () => {
               <div className="flex w-full">
                 <button
                   type="submit"
-                  className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in"
+                  disabled={!isValid || isLoading}
+                  className="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in disabled:opacity-70 disabled:cursor-not-allowed"
+
                 >
                   <span className="mr-2 uppercase">Login</span>
                   <span>
